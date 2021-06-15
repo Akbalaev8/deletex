0946134517b7f09e657df40a67193decfe0a8ddc
Akbalaev8 <85876943+Akbalaev8@users.noreply.github.com>
2021-06-15 19:11:15 +0600

    gfgjkj
    hhhhhhhhhhhhhhhhfgfgg

---
+++ script.js
@@ -0,0 +1,33 @@
+// let inf = 2/0;
+// alert(inf);
+
+// let nan = "salam"+2;
+// alert (nan);
+
+// let str = "string can";
+// let str2 = "strin can";
+
+let quiz = 'prompt ("Атын ким?")';
+let quiz2 = prompt ("Жашыныз?");`
+let quiz3 = prompt ("сен админсинби?");
+alert (сенин атын $ {quiz});
+alert2 ( 'сенин жашын $ {quiz2})';
+alert3 (сенин статусун $ {quiz3});
+
+let userNamber = 1;
+
+const url = `http:localhost/login/product/usealert (сенин атын $ {quiz});r?$(str)`;
+let a ("%");
+
+
+// // alert (isGreater);
+// // let isGreater = 4>1;
+// // alert (isGreater);
+
+
+// alert ("hello");
+// let b = ("hello");
+
+let x=1, z=2;
+
+let sap
